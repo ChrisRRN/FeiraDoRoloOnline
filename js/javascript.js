22709a4e6bb7942d6607b81623f82dfd6ab2ca0b
ChrisRRN <102390156+ChrisRRN@users.noreply.github.com>
2022-08-01 14:10:49 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,137 @@
+
+      
+let enabled = false;
+  function clicador(event) {
+    enabled = !enabled;
+    if (enabled) {
+        previousEl = document.getElementById("clicador");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet = document.getElementById("clicador");
+        objet.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+
+let enabled2 = false;
+  function clicador2(event) {
+    enabled2 = !enabled2;
+    if (enabled2) {
+        previousEl = document.getElementById("clicador2");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet2 = document.getElementById("clicador2");
+        objet2.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+let enabled3 = false;
+  function clicador3(event) {
+    enabled3 = !enabled3;
+    if (enabled3) {
+        previousEl = document.getElementById("clicador3");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet3 = document.getElementById("clicador3");
+        objet3.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+
+let enabled4 = false;
+  function clicador4(event) {
+    enabled4 = !enabled4;
+    if (enabled4) {
+        previousEl = document.getElementById("clicador4");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet4 = document.getElementById("clicador4");
+        objet4.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+
+let enabled5 = false;
+  function clicador5(event) {
+    enabled5 = !enabled5;
+    if (enabled5) {
+        previousEl = document.getElementById("clicador5");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet5 = document.getElementById("clicador5");
+        objet5.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+
+let enabled6 = false;
+  function clicador6(event) {
+    enabled6 = !enabled6;
+    if (enabled6) {
+        previousEl = document.getElementById("clicador6");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet6 = document.getElementById("clicador6");
+        objet6.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+let enabled7 = false;
+  function clicador7(event) {
+    enabled7 = !enabled7;
+    if (enabled7) {
+        previousEl = document.getElementById("clicador7");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet7 = document.getElementById("clicador7");
+        objet7.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+let enabled8 = false;
+  function clicador8(event) {
+    enabled8 = !enabled8;
+    if (enabled8) {
+        previousEl = document.getElementById("clicador8");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet8 = document.getElementById("clicador8");
+        objet8.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+let enabled9 = false;
+  function clicador9(event) {
+    enabled9 = !enabled9;
+    if (enabled9) {
+        previousEl = document.getElementById("clicador9");
+        previousEl.style.fontSize = "150px";
+    } else {
+        let objet9 = document.getElementById("clicador9");
+        objet9.style.color =  "#6E0AD6";
+        previousEl.style.fontSize = "4em";
+        previousEl.style.backgroundColor = "white";
+    }
+}
+
+
+    
+    function alerta2() {
+        window.alert("👏 PRODUTO CADASTRADO COM SUCESSO 👏 ");
+    }
